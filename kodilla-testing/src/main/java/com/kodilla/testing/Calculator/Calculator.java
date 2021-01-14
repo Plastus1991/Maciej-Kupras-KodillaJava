a2dc29cfeb3af9f25220339ac8c3ab8dd365a96e
Maciej Kupras <maciejkupras@wp.pl>
2021-01-14 18:38:31 +0100

    6.2 Added calculator test to com.kodilla.testing

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/Calculator/Calculator.java
@@ -0,0 +1,12 @@
+package com.kodilla.testing.Calculator;
+
+public class Calculator {
+    public int add (int a, int b) {
+        int add = a + b;
+        return add;
+    }
+    public int subtract (int a, int b) {
+        int subtract = a - b;
+        return subtract;
+    }
+}
--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,7 +1,31 @@
 package com.kodilla.testing;
+import com.kodilla.testing.user.SimpleUser;
+import com.kodilla.testing.Calculator.Calculator;
 
 public class TestingMain {
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+
+        String result = simpleUser.getUsername();
+
+        if (result.equals("theForumUser")) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!");
+        }
+
+        System.out.println("Pierwszy test jednostkowy:");
+
+        Calculator calculator = new Calculator();
+        int resultAdd = calculator.add(5, 40);
+        int resultSub = calculator.subtract(33, 44);
+
+        if (resultAdd == 45 && resultSub == -11) {
+            System.out.println("test OK");
+        } else {
+            System.out.println("Error!!");
+        }
+
     }
 }
+
