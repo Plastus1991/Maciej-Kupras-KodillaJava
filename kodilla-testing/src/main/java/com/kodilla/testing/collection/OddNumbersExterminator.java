df81d107922b76ca4699ed106b10679f61c5431c
Maciej Kupras <maciejkupras@wp.pl>
2021-01-19 18:43:16 +0100

    6.3 Collections Test

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -3,9 +3,9 @@ package com.kodilla.testing.collection;
 import java.util.*;
 
 public class OddNumbersExterminator {
-   List<Integer> evenNumbers = new LinkedList<>();
 
     public List<Integer> exterminate(List<Integer> numbers) {
+        List<Integer> evenNumbers = new LinkedList<>();
         for(int i : numbers) {
             if(i%2 == 0) {
                 evenNumbers.add(i);
