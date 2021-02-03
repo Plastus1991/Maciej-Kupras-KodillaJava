6a9f16c871b6b625ac753d22e1113036637b2353
Maciej Kupras <maciejkupras@wp.pl>
2021-02-03 20:09:59 +0100

    7.2_Stream_classForumUser

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -2,6 +2,7 @@ package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemDecorator;
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 import java.util.Locale;
 
@@ -10,11 +11,12 @@ public class StreamMain {
         PoemBeautifier poemBeautifier = new PoemBeautifier();
 
         poemBeautifier.beautify("ttt",(a)->"ABC" + a + "ABC");
-        poemBeautifier.beautify("tarala", (a -> a.toUpperCase(Locale.ROOT)));
-        poemBeautifier.beautify("ABCDEFG", (a -> a.toLowerCase(Locale.ROOT)));
-        poemBeautifier.beautify("tEkST", (a -> a.toLowerCase(Locale.ROOT) + "XYZ"));
+        poemBeautifier.beautify("tarala", (a) -> a.toUpperCase(Locale.ROOT));
+        poemBeautifier.beautify("ABCDEFG", (a) -> a.toLowerCase(Locale.ROOT));
+        poemBeautifier.beautify("tEkST", (a) -> a.toLowerCase(Locale.ROOT) + "XYZ");
 
-
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
 
     }
 }
